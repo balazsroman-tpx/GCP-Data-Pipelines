594e27b340192a2374c8a4ee8e21e312ddf1bb2e
Balazs Roman <balazs.roman@tpximpact.com>
2023-10-22 12:23:15 +0100

    created people based on time_off

--- terraform/environments/infra/functions_hibob.tf
+++ terraform/environments/infra/functions_hibob.tf
@@ -1,4 +1,4 @@
-# --------------------------time_off--------------------------------\
+# --------------------------time_off--------------------------------
 # Generates an archive of the source code compressed as a .zip file.
 data "archive_file" "hibob_time_off" {
   type        = "zip"
@@ -37,6 +37,51 @@ resource "google_cloudfunctions_function" "hibob_time_off" {
   environment_variables = {
     "DATASET_ID"           = google_bigquery_dataset.hibob_raw.dataset_id
     "TABLE_NAME"           = google_bigquery_table.time_off.table_id
+    "TABLE_LOCATION"       = google_bigquery_dataset.hibob_raw.location
+    "GOOGLE_CLOUD_PROJECT" = var.project
+
+  }
+}
+
+# ---------------------------people---------------------------------
+# Generates an archive of the source code compressed as a .zip file.
+data "archive_file" "hibob_people" {
+  type        = "zip"
+  source_dir  = "../../../cloud_functions/hibob/people"
+  output_path = "/tmp/hibob_people.zip"
+}
+
+# Add source code zip to the Cloud Function's bucket
+resource "google_storage_bucket_object" "hibob_people" {
+  source       = data.archive_file.hibob_people.output_path
+  content_type = "application/zip"
+
+  # Append to the MD5 checksum of the files's content
+  # to force the zip to be updated as soon as a change occurs
+  name   = "cloud_function-${data.archive_file.hibob_people.output_md5}.zip"
+  bucket = data.google_storage_bucket.function_bucket.name
+}
+
+resource "google_cloudfunctions_function" "hibob_people" {
+  name                = "hibob_people_pipe"
+  runtime             = "python39" # of course changeable
+  available_memory_mb = 1024
+  timeout             = 540
+  # Get the source code of the cloud function as a Zip compression
+  source_archive_bucket = data.google_storage_bucket.function_bucket.name
+  source_archive_object = google_storage_bucket_object.hibob_people.name
+
+  # Must match the function name in the cloud function `main.py` source code
+  entry_point                  = "main"
+  https_trigger_security_level = "SECURE_ALWAYS"
+  event_trigger {
+    event_type = "providers/cloud.pubsub/eventTypes/topic.publish"
+    resource   = google_pubsub_topic.cloud_function_trigger_hot.id
+  }
+
+  environment_variables = {
+    "DATASET_ID"           = google_bigquery_dataset.hibob_raw.dataset_id
+    "TABLE_NAME"           = google_bigquery_table.people.table_id
     "TABLE_LOCATION"       = google_bigquery_dataset.hibob_raw.location
     "GOOGLE_CLOUD_PROJECT" = var.project
 
