9256d0454657608cf192b7c63e906eb683f9e014
Balazs Roman <balazs.roman@tpximpact.com>
2024-04-15 16:58:20 +0100

    times updated for cloud_scheduler resources

--- terraform/environments/infra/trigger.tf
+++ terraform/environments/infra/trigger.tf
@@ -6,13 +6,13 @@ resource "google_pubsub_topic" "cloud_function_trigger_cold" {
 
 resource "google_cloud_scheduler_job" "daily-5-45" {
   name        = "daily-5-45-trigger"
-  description = "Scheduled daily to trigger cloud function at 5:45"
-  schedule    = "*/45 5 * * *"
+  description = "Scheduled daily to trigger cloud function at 05:45"
+  schedule    = "45 5 * * *"
 
   pubsub_target {
     # topic.id is the topic's full resource name.
     topic_name = google_pubsub_topic.cloud_function_trigger_cold.id
-    data       = base64encode("weekly")
+    data       = base64encode("daily")
   }
 }
 
@@ -21,15 +21,15 @@ resource "google_pubsub_topic" "cloud_function_trigger_hot" {
   kms_key_name = google_kms_crypto_key.pub_sub_key.id
 }
 
-resource "google_cloud_scheduler_job" "two-hourly-00" {
-  name        = "every-two-hours-00"
-  description = "Scheduled to trigger cloud function every two hours on the hour"
-  schedule    = "0 */2 * * *"
+resource "google_cloud_scheduler_job" "hourly-00" {
+  name        = "every-hour-00"
+  description = "Scheduled to trigger cloud function every hour from 6 through 18"
+  schedule    = "0 6-18 * * *"
 
   pubsub_target {
     # topic.id is the topic's full resource name.
     topic_name = google_pubsub_topic.cloud_function_trigger_hot.id
-    data       = base64encode("2hr")
+    data       = base64encode("01hr00min")
   }
 }
 
@@ -38,14 +38,14 @@ resource "google_pubsub_topic" "cloud_function_trigger_hot_2" {
   kms_key_name = google_kms_crypto_key.pub_sub_key.id
 }
 
-resource "google_cloud_scheduler_job" "two-hourly-15" {
-  name        = "every-two-hours-15"
-  description = "Scheduled to trigger cloud function every two hours 15 minutes past each hour"
-  schedule    = "15 */2 * * *"
+resource "google_cloud_scheduler_job" "hourly-15" {
+  name        = "every-hour-15"
+  description = "Scheduled to trigger cloud function 15 past every hour from 6 through 18"
+  schedule    = "15 6-18 * * *"
 
   pubsub_target {
     # topic.id is the topic's full resource name.
     topic_name = google_pubsub_topic.cloud_function_trigger_hot_2.id
-    data       = base64encode("2hr15")
+    data       = base64encode("01hr15min")
   }
 }
